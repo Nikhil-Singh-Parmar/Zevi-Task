07327e5452a387aeeded80e85d6b9087518ffa95
Nikhil Singh Parmar <nichil942singh@gmail.com>
2022-09-06 00:42:16 +0530

    adding search bar in the landing page

--- zevi/src/App.js
+++ zevi/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import { RiSearchLine } from 'react-icons/ri';
 function App() {
   return (
     <>
     <div className='homepage'>
-
+     <div className='body'>
+     <div className='searchbar'>
+      <input className='input searchbar' type='text' placeholder = 'Search'></input>
+      <RiSearchLine className='icon'></RiSearchLine>
+     </div>
+     </div>
     </div>
     </>
   );
