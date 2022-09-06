180b0c941a0b109d72b4e1ae0262a3a4eca2ddc3
Nikhil Singh Parmar <nichil942singh@gmail.com>
2022-09-06 15:36:35 +0530

    setting up result screen and relevant state

--- zevi/src/App.js
+++ zevi/src/App.js
@@ -4,6 +4,7 @@ import { RiSearchLine } from 'react-icons/ri';
 function App() {
   const [searchData,setsearchData] = useState("");
   const [suggestion,setsuggestion] = useState(false);
+  const [activeSearch,setactiveSearch] = useState(true);
 
   function handleClick(){
     setsuggestion(!suggestion);
@@ -11,14 +12,43 @@ function App() {
   }
   return (
     <>
-    <div className='homepage'>
+  {activeSearch?
+  <div>
+    <div className='searchbar-mini'>
+      <input
+       className='input-mini searchbar-mini' 
+       type='text' 
+       placeholder = 'Search'
+       onChange={e=>setsearchData(e.target.value)}></input>
+      <RiSearchLine 
+      className='icon-mini'
+       onClick={handleClick}></RiSearchLine>
+     </div>
+     <div className='search-result-body'>
+        <div className='sidebar'>
+
+        </div>
+        <div className='result-data'>
+
+        </div>
+     </div>
+  </div>
+  : <div>
+   <div className='homepage'>
      <div className='body'>
      <div className='searchbar'>
-      <input className='input searchbar' type='text' placeholder = 'Search' onChange={e=>setsearchData(e.target.value)}></input>
-      <RiSearchLine className='icon' onClick={handleClick} ></RiSearchLine>
+      <input
+       className='input searchbar' 
+       type='text' 
+       placeholder = 'Search'
+       onChange={e=>setsearchData(e.target.value)}></input>
+      <RiSearchLine 
+      className='icon'
+       onClick={handleClick}></RiSearchLine>
      </div>
      </div>
     </div>
+   </div>}
     </>
   );
 }
