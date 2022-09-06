77ea2321499fc797129f807483f837e50b5f4cb0
Nikhil Singh Parmar <nichil942singh@gmail.com>
2022-09-06 12:34:12 +0530

    adding state to input form

--- zevi/src/App.js
+++ zevi/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import React,{useState}  from 'react'
 import { RiSearchLine } from 'react-icons/ri';
 function App() {
+  const [searchData,setsearchData] = useState("");
+  const [suggestion,setsuggestion] = useState(false);
+
+  function handleClick(){
+    setsuggestion(!suggestion);
+    console.log(suggestion);
+  }
   return (
     <>
     <div className='homepage'>
      <div className='body'>
      <div className='searchbar'>
-      <input className='input searchbar' type='text' placeholder = 'Search'></input>
-      <RiSearchLine className='icon'></RiSearchLine>
+      <input className='input searchbar' type='text' placeholder = 'Search' onChange={e=>setsearchData(e.target.value)}></input>
+      <RiSearchLine className='icon' onClick={handleClick} ></RiSearchLine>
      </div>
      </div>
     </div>
